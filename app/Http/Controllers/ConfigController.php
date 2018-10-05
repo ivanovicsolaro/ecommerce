095ba6b66d34af6b33358d3cdc3bb3eef7f72540
Usuario <Usuario@192.168.1.5>
2018-10-05 14:31:46 -0300

    banners terminado

--- app/Http/Controllers/ConfigController.php
+++ app/Http/Controllers/ConfigController.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Input;
 
 class ConfigController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth');
+    }
+    
     public function indexBanner(){
         $banner = Banner::find(1);
 
